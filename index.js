86faa9d4b783ef92f5f6baae97cf50e9bd2821a1
emregency <banzai1987@gmail.com>
2020-07-08 22:26:57 +0200

    fix: that was a stupid ommision

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const vercelPreviewUrl = core.getInput("vercel-preview-url", {
   required: true
 });
 const vercelTargetUrl = core.getInput("vercel-target-url", { required: true });
-const vercelScope = core.getInput("scope");
+const vercelScope = core.getInput("vercel-scope");
 
 async function vercelAlias() {
   let myOutput = "";
